0bf8846574a3d6e9e8a73433982743b9b63ad39b
hana <asufanah@gmail.com>
2015-02-19 23:35:44 +0900

    Add fields to User resource.

--- src/main/java/com/github/asufana/resources/User.java
+++ src/main/java/com/github/asufana/resources/User.java
@@ -1,8 +1,13 @@
 package com.github.asufana.resources;
 
+import java.util.*;
+
 import lombok.*;
 import lombok.Value;
 import lombok.experimental.*;
+
+import org.joda.time.*;
+import org.joda.time.format.*;
 
 @Value
 @Accessors(fluent = true)
@@ -10,4 +15,51 @@ import lombok.experimental.*;
 public class User {
     
     private Integer id;
+    private String role;
+    private String media_device_id;
+    private String status;
+    private String message;
+    private String name;
+    private String email;
+    private String avatar;
+    private String snapshot;
+    private Integer snapshot_interval;
+    private boolean confirmed;
+    private String time_zone;
+    private String time_zone_offset;
+    private String created_at;
+    private String last_active_at;
+    private String last_connected_at;
+    
+    public Role role() {
+        return Arrays.asList(Role.values())
+                     .stream()
+                     .filter(r -> r.name().toLowerCase().equals(role))
+                     .findAny()
+                     .get();
+    }
+    
+    public DateTime created_at() {
+        return toDateTime(created_at);
+    }
+    
+    public DateTime last_active_at() {
+        return toDateTime(last_active_at);
+    }
+    
+    public DateTime last_connected_at() {
+        return toDateTime(last_connected_at);
+    }
+    
+    private DateTime toDateTime(final String isoDateTimeFormatString) {
+        return ISODateTimeFormat.dateTime()
+                                .parseDateTime(isoDateTimeFormatString);
+    }
+    
+    public static enum Role {
+        user,
+        owner,
+        manager,
+        banned;
+    }
 }
