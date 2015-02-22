a1b50bf802b93c8c0d39eeb55d2aafb9041aaaf2
hana <asufanah@gmail.com>
2015-02-22 20:25:03 +0900

    Add fields to User resource.

--- src/main/java/com/github/asufana/resources/User.java
+++ src/main/java/com/github/asufana/resources/User.java
@@ -1,79 +1,74 @@
 package com.github.asufana.resources;
 
-import java.util.*;
-
 import lombok.*;
 import lombok.Value;
 import lombok.experimental.*;
 
 import org.joda.time.*;
-import org.joda.time.format.*;
+
+import com.google.gson.annotations.*;
 
 @Value
 @Accessors(fluent = true)
+@EqualsAndHashCode(callSuper = true)
 @ToString
-public class User {
+public class User extends AbstractResource {
     
     private Integer id;
     private String role;
-    private String media_device_id;
+    @SerializedName("media_device_id")
+    private String mediaDeviceId;
     private String status;
     private String message;
     private String name;
     private String email;
     private String avatar;
     private String snapshot;
-    private Integer snapshot_interval;
+    @SerializedName("snapshot_interval")
+    private Integer snapshotInterval;
     private boolean confirmed;
-    private String time_zone;
-    private String time_zone_offset;
-    private String created_at;
-    private String last_active_at;
-    private String last_connected_at;
+    @SerializedName("is_contact")
+    private boolean isContact;
+    private boolean support;
+    @SerializedName("idle_at")
+    private String idleAt;
+    private Contact contact;
     
-    public Role role() {
-        return Arrays.asList(Role.values())
-                     .stream()
-                     .filter(r -> r.name().toLowerCase().equals(role))
-                     .findAny()
-                     .get();
+    @SerializedName("time_zone")
+    private String timeZone;
+    @SerializedName("time_zone_offset")
+    private String timeZoneOffset;
+    @SerializedName("created_at")
+    private String createdAt;
+    @SerializedName("last_active_at")
+    private String lastActiveAt;
+    @SerializedName("last_connected_at")
+    private String lastConnectedAt;
+    
+    @Value
+    @Accessors(fluent = true)
+    @ToString
+    public static class Contact {
+        private Integer id;
+        private String state;
+        @SerializedName("user_id")
+        private Integer userId;
+        @SerializedName("contact_id")
+        private Integer contactId;
+        @SerializedName("created_at")
+        private String createdAt;
     }
     
-    public Status status() {
-        return Arrays.asList(Status.values())
-                     .stream()
-                     .filter(r -> r.name().toLowerCase().equals(status))
-                     .findAny()
-                     .get();
+    public DateTime createdAt() {
+        return toDateTime(createdAt);
     }
     
-    public DateTime created_at() {
-        return toDateTime(created_at);
+    public DateTime lastActiveAt() {
+        return toDateTime(lastActiveAt);
     }
     
-    public DateTime last_active_at() {
-        return toDateTime(last_active_at);
-    }
-    
-    public DateTime last_connected_at() {
-        return toDateTime(last_connected_at);
-    }
-    
-    private DateTime toDateTime(final String isoDateTimeFormatString) {
-        return ISODateTimeFormat.dateTime()
-                                .parseDateTime(isoDateTimeFormatString);
-    }
-    
-    public static enum Role {
-        user,
-        owner,
-        manager,
-        banned;
-    }
-    
-    public static enum Status {
-        busy,
-        available;
+    public DateTime lastConnectedAt() {
+        return toDateTime(lastConnectedAt);
     }
     
 }
